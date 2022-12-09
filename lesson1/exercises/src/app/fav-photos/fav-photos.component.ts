d470d9da824efd246abc55e1a80d621636f9c969
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-12-08 18:36:18 -0600

    added exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.imgur.com/4QeNsCD.jpeg';
+  image3 = 'https://i.imgur.com/r2756Qd.jpeg';
 
   constructor() { }
 
